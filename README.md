2db65f680711f8ad347fda65b445214bae6c213b
Matt Robson <mrobson@redhat.com>
2015-12-07 15:22:30 -0500

    Adding fuse-base-os dockerfile

---
+++ README.md
@@ -0,0 +1,3 @@
+fuse-base-os
+------------
+Base OS, builds on the base JDK image.  Adds users, directories, OS level tuning and parameters.
---
+++ build.sh
@@ -0,0 +1,5 @@
+DOCKER_IMAGE_NAME=mrobson/fuse-base-os
+DOCKER_IMAGE_VERSION=latest
+
+docker rmi --force=true ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}
+docker build --force-rm=true --rm=true -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION} .
